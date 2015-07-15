0904f7b47e66e0313ba4c2034316b2e9cb4ded1f
Emily Landi <emily.landi@gmail.com>
2015-07-15 15:00:40 -0700

    added event to add new shop. right now it adds the same new shop over and over rather than updating existing store

--- shops.js
+++ shops.js
@@ -8,6 +8,15 @@ var Shop = function (name, minCust, maxCust, avDonuts) {
   this.total = 0;
 };
 
+//Creates total donuts per hour(based on random number of customers and average donuts bought) for 11 hours and places values in an array by using.push
+Shop.prototype.donutsPerHour = function() {
+  var hourly = [];
+  for (var i = 0; i < this.hoursOpen; i++) {
+    hourly.push(Math.floor(((Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust) * this.avDonuts));
+  }
+  return hourly;
+};
+
 //Displays info on webpage
 Shop.prototype.display = function () {
   var table = document.getElementById("table");
@@ -32,23 +41,59 @@ Shop.prototype.display = function () {
   tr.appendChild(totalDonuts);
 };
 
-//Creates total donuts per hour(based on random number of customers and average donuts bought) for 11 hours and places values in an array by using.push
-Shop.prototype.donutsPerHour = function() {
-  var hourly = [];
-  for (var i = 0; i < this.hoursOpen; i++) {
-    hourly.push(Math.floor(((Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust) * this.avDonuts));
-  }
-  return hourly;
-};
+//This puts all of the different shop objects into an array
+var locationArray = [];
 
-var downtown = new Shop ("Downtown", 8, 43, 4.5);
-var capitolHill = new Shop ("Capitol Hill", 4, 37, 2);
-var southLakeUnion = new Shop ("South Lake Union", 9, 23, 6.33);
-var wedgewood = new Shop ("Wedgewood", 2, 28, 1.25);
-var ballard = new Shop ("Ballard", 8, 58, 3.75);
+locationArray.push(new Shop ("Downtown", 8, 43, 4.5));
+locationArray.push(new Shop ("Capitol Hill", 4, 37, 2));
+locationArray.push(new Shop ("South Lake Union", 9, 23, 6.33));
+locationArray.push(new Shop ("Wedgewood", 2, 28, 1.25));
+locationArray.push(new Shop ("Ballard", 8, 58, 3.75));
 
-downtown.display();
-capitolHill.display();
-southLakeUnion.display();
-wedgewood.display();
-ballard.display();
+for (var i=0; i < locationArray.length; i++) {
+  locationArray[i].display();
+}
+
+//This adds the same new shop over and over and over...need to fix so that user can enter multiple new stores
+var addNewShop = document.getElementById("addNewShop");
+addNewShop.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var newLocation = document.getElementById("newLocation").value;
+  var newMin = parseInt(document.getElementById("newMin").value);
+  var newMax = parseInt(document.getElementById("newMax").value);
+  var newAv = parseInt(document.getElementById("newAv").value);
+
+  //This will add new shop to table. Over & Over.
+  locationArray.push(new Shop(newLocation, newMin, newMax, newAv));
+  locationArray[i].display();
+
+  // //This should check if the location already exists.
+  // for (i = 0; i < locationArray.length; i++) {
+  //   //If the new location does not exist, add it to the table.
+  //   if (newLocation !== locationArray[i].name) {
+  //     locationArray.push(new Shop(newLocation, newMin, newMax, newAv));
+  //     locationArray[locationArray.length - 1].display();
+  //   }
+
+  // //   If the new location already exists, delete it.
+  // //   if (check === true) {
+  // //   code here;
+  // //   }
+  // }
+}, false);
+
+
+
+
+
+// var downtown = new Shop ("Downtown", 8, 43, 4.5);
+// var capitolHill = new Shop ("Capitol Hill", 4, 37, 2);
+// var southLakeUnion = new Shop ("South Lake Union", 9, 23, 6.33);
+// var wedgewood = new Shop ("Wedgewood", 2, 28, 1.25);
+// var ballard = new Shop ("Ballard", 8, 58, 3.75);
+
+// downtown.display();
+// capitolHill.display();
+// southLakeUnion.display();
+// wedgewood.display();
+// ballard.display();
